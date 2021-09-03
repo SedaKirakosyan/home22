5ccd40c36b9d8071ebbbed89ec64a0817341f812
SedaKirakosyan <89702829+SedaKirakosyan@users.noreply.github.com>
2021-09-03 22:45:16 +0400

    Add files via upload

---
+++ corr.py
@@ -0,0 +1,19 @@
+"""1.Write a python function to 
+create a simple Calculator."""
+# def cal(x):
+# 	d = eval(x)
+# 	return d
+# x = input("")
+# print(cal(x))
+
+"""5.Write a python program
+ to sum all letter and 
+number in your string."""
+# def word(jj,c):
+# 	for i in jj:
+# 		if i.isdigit():
+# 			c +=1
+# 	print(f"numbers {c} and letters {len(jj)-c}")
+# jj = input("say a word: ").split()
+# c = 0
+# word(jj,c)
